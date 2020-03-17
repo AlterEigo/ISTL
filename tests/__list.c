6df1d1f7102f6d3f36a8f5a287d4954135fb8419
Iaroslav Sorokin <iaroslav.sorokin@epitech.eu>
2020-03-17 02:06:42 +0100

    Add: template for list criterion test.

---
+++ tests/__list.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** Title
+** File description:
+** Description
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+
+#include "istl/list.h"
+
+Test(list_basic, create_destroy_list)
+{
+
+}
